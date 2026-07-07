5793f96c9cb620f36418ff1b86b142d08fb212bc
CodowaveAI <CodowaveAI-bot@users.noreply.github.com>
2026-07-07 12:46:13 +0000

    fix(#1016): [type/docs] Document 1 export in src/to-celsius.ts

--- src/to-celsius.ts
+++ src/to-celsius.ts
@@ -1,3 +1,8 @@
+/**
+ * Converts a temperature from Fahrenheit to Celsius.
+ * @param f Temperature in degrees Fahrenheit.
+ * @returns Equivalent temperature in degrees Celsius.
+ */
 export function toCelsius(f: number): number {
   return ((f - 32) * 5) / 9;
 }
